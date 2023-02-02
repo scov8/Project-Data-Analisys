2fc9aebe328795717f22ef679eef1d0b082198c9
Francesco Tortora <francescotrtr@gmail.com>
2023-02-02 11:37:25 +0100

    add plot

--- Pt.1/Parte1.R
+++ Pt.1/Parte1.R
@@ -134,6 +134,63 @@ min <- which.min(val.errors) # Index of the minimum MSE value
 hyb.mse <- val.errors[min] # Stores the minimum MSE value in 'hyb.mse'
 hyb.coef <- coef(hyb.regfit, which.min(val.errors)) # Extracts the coefficients for the best model obtained from hybrid selection
 
+######################## Plot ########################
+
+###### LI LASCIAMO???????????????????????!!!!
+dev.new()
+# pdf(file="backward_cp_v20.pdf", width=20, height=10)
+plot(bwd.regfit, scale = "Cp")
+title("Backward selection with Cp")
+# dev.off()
+
+dev.new()
+# pdf(file="forward_cp_v20.pdf", width=20, height=10)
+plot(fwd.regfit, scale = "Cp")
+title("Forward selection with Cp")
+# dev.off()
+
+dev.new()
+# pdf(file="hybrid_cp_v20.pdf", width=20, height=10)
+plot(hyb.regfit, scale = "Cp")
+title("Hybrid selection with Cp")
+# dev.off()
+
+dev.new()
+# pdf(file="backward_bic_v20.pdf", width=20, height=10)
+plot(bwd.regfit, scale = "bic")
+title("Backward selection with bic")
+# dev.off()
+
+dev.new()
+# pdf(file="forward_bic_v20.pdf", width=20, height=10)
+plot(fwd.regfit, scale = "bic")
+title("Forward selection with bic")
+# dev.off()
+
+dev.new()
+# pdf(file="hybrid_bic_v20.pdf", width=20, height=10)
+plot(hyb.regfit, scale = "bic")
+title("Hybrid selection with bic")
+# dev.off()
+
+dev.new()
+# pdf(file="backward_adjr_v20.pdf", width=20, height=10)
+plot(bwd.regfit, scale = "adjr2")
+title("Backward selection with adjr2")
+# dev.off()
+
+dev.new()
+# pdf(file="forward_adjr_v20.pdf", width=20, height=10)
+plot(fwd.regfit, scale = "adjr2")
+title("Forward selection with adjr2")
+# dev.off()
+
+dev.new()
+# pdf(file="hybrid_adjr_v20.pdf", width=20, height=10)
+plot(hyb.regfit, scale = "adjr2")
+title("Hybrid selection with adjr2")
+# dev.off()
+
 ################### Print Results ##################
 bwd.predictors <- round(bwd.coef / 100, digits = 0) # Round the values in bwd.coef and divide by 100, with 0 decimal places
 intToUtf8(bwd.predictors) # Convert the values in bwd.predictors to UTF-8 encoded integers
